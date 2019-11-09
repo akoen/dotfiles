37a7f5c010c39c1ca61132f8829a9fe6342d62a6
Alex Koen <arw.koen@gmail.com>
2019-11-08 18:57:09 -0800

    Cleaned up environment variables

--- zsh/.zsh/01-environment.zsh
+++ zsh/.zsh/01-environment.zsh
@@ -1,5 +1,6 @@
 # environment variables
-export EDITOR='nvim'
+export EDITOR='emacsclient -nw'
+
 export PATH=$HOME/bin:$PATH
 export MOZ_USE_XINPUT2=1
 export GDk_SCALE=2
@@ -9,7 +10,3 @@ export ANDROID_HOME=~/Android/
 export PATH=$PATH:~/Android/platform-tools/
 export PATH=$PATH:~/Android/tools
 export PATH=$PATH:~/Android/build-tools/29.0.2/
-
-# fzf
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -1,3 +1,8 @@
 # Use fzf with rg
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Source fzf keybindings
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
 [ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
+
