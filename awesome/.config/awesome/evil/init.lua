cf75b2d1c49dac7db2c04849fe7e84c42f993e0f
Alex Koen <arw.koen@gmail.com>
2019-12-28 10:22:07 -0800

    Initial awesomewm commit

---
+++ awesome/.config/awesome/evil/init.lua
@@ -0,0 +1,13 @@
+-- evil = {}
+
+require("evil.volume")
+require("evil.battery")
+require("evil.cpu")
+require("evil.temperature")
+require("evil.ram")
+require("evil.weather")
+require("evil.mpd")
+require("evil.brightness")
+require("evil.spotify")
+
+-- return evil
