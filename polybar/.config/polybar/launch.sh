e97fdf756a784d93f84db3ea03f86b2d735bfd26
akoen <arw.koen@gmail.com>
2020-05-22 13:57:05 -0700

    Polybar colours

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,5 +8,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
+polybar main -c ~/.config/polybar/config &
