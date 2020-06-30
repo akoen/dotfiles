019bb0773fa93e5b752b9bc2521cf7c57ac59346
akoen <arw.koen@gmail.com>
2020-06-29 20:19:18 -0700

    fzf fd

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -1,8 +1,9 @@
-# Use fzf with rg
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow 2> /dev/null'
+# Use fzf with fd
+
+export FZF_DEFAULT_COMMAND='fd -t f --follow --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd -t d"
 
 # Source fzf keybindings
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
 [ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
-
