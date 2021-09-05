d72e1d7188f5fb95b3fe93a227978cbbea1eda9d
akoen <arw.koen@gmail.com>
2021-09-05 09:41:47 -0700

    Remove elodie

--- zsh/.config/zsh/06-aliases.zsh
+++ zsh/.config/zsh/06-aliases.zsh
@@ -28,28 +28,3 @@ alias xclip="xclip -selection c"
 alias nnn='NNN_FIFO="$(mktemp -u)" nnn'
 
 alias doom='emacs --with-profile doom'
-
-## Photos
-alias image='feh -g 640x480 --scale-down --auto-rotate --start-at'
-alias elodie='~/.local/bin/elodie/elodie.py'
-alias pi='elodie import --debug --destination="~/Pictures/Elodie"'
-alias ph+='exiftool -AllDates+=1'
-alias ph-='exiftool -AllDates-=1'
-alias pu='elodie update --debug'
-
-pua() {
-    elodie update --album="$1" ${@:2}
-}
-
-pu-7() {
-    for file in "$@"
-    do
-        echo $FILE | grep -o -P "(?<=/)\d*-\d*-\d*_\d*-\d*-\d*" 
-    done
-}
-
-# Does not work
-# put() {
-#     ~/Downloads/elodie/elodie.py update --debug --time="$(date -d "$(xclip -o | sed 's/,//g')" '+%F %T')"
-# }
-
