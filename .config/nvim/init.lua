7629f5b774938980d7c6fb950e8d3d85887cff51
akoen <arw.koen@gmail.com>
2022-10-09 10:42:31 -0700

    2022-10-09

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -191,7 +191,7 @@ vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_view_general_viewer = 'zathura'
 
 -- Gilles Castel LaTeX setup
-vim.cmd([[nnoremap  <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>]])
+vim.cmd([[nnoremap <leader>lc : silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>]])
 vim.cmd([[nnoremap <leader>le : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]])
 
 --- clipboard-image
